33efdd7e647ef390ad9101a5d39bd720b4c6eec5
Alexander Kostrikin <aleksandrkostrikin086@gmail.com>
2023-05-01 16:57:26 +0300

    Improve Block System

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -4,12 +4,90 @@ import Block from "./block/Block";
 
 export default class Main extends React.Component {
     render() {
+        const dataMock = [
+            {
+                title: 'Backlog',
+                issues: [
+                ]
+            }, 
+            {
+                title: 'Ready',
+                issues: [
+                    {
+                        id: '2',
+                        name: 'Login page – performance issues',
+                        description: 'Fix all the perfomance issues'
+                    },
+                    {
+                        id: '3',
+                        name: 'Login page – performance issues',
+                        description: 'Fix all the perfomance issues'
+                    },                     {
+                        id: '2',
+                        name: 'Login page – performance issues',
+                        description: 'Fix all the perfomance issues'
+                    },
+                    {
+                        id: '3',
+                        name: 'Login page – performance issues',
+                        description: 'Fix all the perfomance issues'
+                    },
+                    {
+                        id: '2',
+                        name: 'Login page – performance issues',
+                        description: 'Fix all the perfomance issues'
+                    },
+                    {
+                        id: '3',
+                        name: 'Login page – performance issues',
+                        description: 'Fix all the perfomance issues'
+                    },
+                    {
+                        id: '2',
+                        name: 'Login page – performance issues',
+                        description: 'Fix all the perfomance issues'
+                    },
+                    {
+                        id: '3',
+                        name: 'Login page – performance issues',
+                        description: 'Fix all the perfomance issues'
+                    }
+                ]
+            },
+            {
+                title: 'In Progress',
+                issues: [
+                    {
+                        id: '4',
+                        name: 'Login page – performance issues',
+                        description: 'Fix all the perfomance issues'
+                    },
+                    {
+                        id: '5',
+                        name: 'Login page – performance issues',
+                        description: 'Fix all the perfomance issues'
+                    }
+                ]
+            },
+            {
+                title: 'Finished',
+                issues: [
+                    {
+                        id: '6',
+                        name: 'Login page – performance issues',
+                        description: 'Fix all the perfomance issues'
+                    },
+                    {
+                        id: '7',
+                        name: 'Login page – performance issues',
+                        description: 'Fix all the perfomance issues'
+                    }
+                ]
+            }
+        ]
         return(
             <main>
-                <Block name="Backlog" />
-                <Block name="Ready" />
-                <Block name="In Progress" />
-                <Block name="Finished" />
+                {dataMock.map(block => (<Block title={block.title} issues={block.issues} />))}
             </main>
         );
     }
--- src/components/main/block/Block.jsx
+++ src/components/main/block/Block.jsx
@@ -4,25 +4,32 @@ import plus from "./plus.svg";
 
 export default class Block extends React.Component {
     render() {
+        const {title, issues} = this.props;
+
+        const AddBacklog = () => {
+            return <input type="text" name="" id="" />
+        }
+
+        const AddAnother = () => {
+            return <input type="button" name="" id="" />
+        }
+
+        const handleClick = (e) => {
+            e.preventDefault();
+            if (e.target.parentNode.previousElementSibling.textContent == "Backlog") {
+                console.log(1);
+            } else {
+                console.log(e.target.parentNode.previousElementSibling);
+            }
+        }
         return(
-            <div className="block">
-                <h2>{this.props.name}</h2>
+            <div className="block" id="title">
+                <h2>{title}</h2>
                 <div className="wrapper">
-                    <div className="content">
-                        <p className="content-card">Login page – performance issues</p>
-                        <p className="content-card">Login page – performance issues</p>
-                        <p className="content-card">Login page – performance issues</p>
-                        <p className="content-card">Login page – performance issues</p>
-                        <p className="content-card">Login page – performance issues</p>
-                        <p className="content-card">Login page – performance issues</p>
-                        <p className="content-card">Login page – performance issues</p>
-                        <p className="content-card">Login page – performance issues</p>
-                        <p className="content-card">Login page – performance issues</p>
-                        <p className="content-card">Login page – performance issues</p>
-                        <p className="content-card">Login page – performance issues</p>
-                        <p className="content-card">Login page – performance issues</p>
-                    </div>
-                    <a href="#" className="add">
+                    {issues.map(issue => (
+                        <p className="content-card" id={issue.id}>{issue.name}</p>
+                    ))}
+                    <a href="#" className="add" onClick={(e) => handleClick(e)}>
                         <img src={plus} alt="Plus Icon" />
                         Add Card
                     </a>
